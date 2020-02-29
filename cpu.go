078e49324d642dbf14b467d80c290c219923a1dc
mohanson <mohanson@outlook.com>
2020-02-29 20:51:28 +0800

    Submit for stash 27

--- cpu.go
+++ cpu.go
@@ -5,3 +5,17 @@ type CPU struct {
 	Memory   []byte
 	PC       uint64
 }
+
+func (c *CPU) SetRegister(i int, u uint64) {
+	if i == Rzero {
+		return
+	}
+	c.Register[i] = u
+}
+
+func (c *CPU) GetRegister(i int) uint64 {
+	if i == Rzero {
+		return 0x00
+	}
+	return c.Register[i]
+}
