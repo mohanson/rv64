a7919411ccb4abc3b9035d9ff18aa4cf111b7ed6
mohanson <mohanson@outlook.com>
2020-03-01 21:05:28 +0800

    Submit for stash 30

--- base_instruction_set_rv64i.go
+++ base_instruction_set_rv64i.go
@@ -37,49 +37,58 @@ func ExecuterRV64I(c *CPU, i uint64) (int, error) {
 		rd, rs1, imm := IType(i)
 		imm = InstructionPart(imm, 0, 5)
 		DebuglnIType("SLLI", rd, rs1, imm)
-		c.Register[rd] = c.Register[rs1] << imm
+		c.SetRegister(rd, c.GetRegister(rs1)<<imm)
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1100_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0101_0000_0001_0011: // SRLI
 		rd, rs1, imm := IType(i)
 		imm = InstructionPart(imm, 0, 5)
 		DebuglnIType("SRLI", rd, rs1, imm)
-		c.Register[rd] = c.Register[rs1] >> imm
+		c.SetRegister(rd, c.GetRegister(rs1)>>imm)
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1100_0000_0000_0111_0000_0111_1111 == 0b_0100_0000_0000_0000_0101_0000_0001_0011: // SRAI
 		rd, rs1, imm := IType(i)
 		imm = InstructionPart(imm, 0, 5)
 		DebuglnIType("SRAI", rd, rs1, imm)
-		c.Register[rd] = uint64(int64(c.Register[rs1]) >> imm)
+		c.SetRegister(rd, uint64(int64(c.GetRegister(rs1))>>imm))
 		c.PC += 4
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0000_0000_0001_1011: // ADDIW
 		rd, rs1, imm := IType(i)
 		imm = SignExtend(imm, 11)
 		DebuglnIType("ADDIW", rd, rs1, imm)
-		c.Register[rd] = uint64(int32(c.Register[rs1]) + int32(imm))
+		c.SetRegister(rd, uint64(int32(c.GetRegister(rs1))+int32(imm)))
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1110_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0001_0000_0001_1011: // SLLIW
 		rd, rs1, imm := IType(i)
-		imm = InstructionPart(imm, 0, 5)
+		if InstructionPart(imm, 5, 5) != 0x00 {
+			return 0, ErrAbnormalInstruction
+		}
+		imm = InstructionPart(imm, 0, 4)
 		DebuglnIType("SLLIW", rd, rs1, imm)
-		c.Register[rd] = SignExtend(uint64(uint32(c.Register[rs1]<<imm)), 31)
+		c.SetRegister(rd, SignExtend(uint64(int32(c.GetRegister(rs1))<<imm), 31))
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1110_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0101_0000_0001_1011: // SRLIW
 		rd, rs1, imm := IType(i)
-		imm = InstructionPart(imm, 0, 5)
+		if InstructionPart(imm, 5, 5) != 0x00 {
+			return 0, ErrAbnormalInstruction
+		}
+		imm = InstructionPart(imm, 0, 4)
 		DebuglnIType("SRLIW", rd, rs1, imm)
-		c.Register[rd] = SignExtend(uint64(uint32(c.Register[rs1]>>imm)), 31)
+		c.SetRegister(rd, SignExtend(uint64(int32(c.GetRegister(rs1))>>imm), 31))
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1110_0000_0000_0111_0000_0111_1111 == 0b_0100_0000_0000_0000_0101_0000_0001_1011: // SRAIW
 		rd, rs1, imm := IType(i)
-		imm = InstructionPart(imm, 0, 5)
+		if InstructionPart(imm, 5, 5) != 0x00 {
+			return 0, ErrAbnormalInstruction
+		}
+		imm = InstructionPart(imm, 0, 4)
 		DebuglnIType("SRAIW", rd, rs1, imm)
-		c.Register[rd] = uint64(int64(c.Register[rs1]) >> imm)
+		c.SetRegister(rd, uint64(int32(c.GetRegister(rs1))>>imm))
 		c.PC += 4
 		return 1, nil
 	case i&0b_1111_1110_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0000_0000_0011_1011: // ADDW
