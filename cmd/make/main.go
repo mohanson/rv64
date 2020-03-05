527c03a22ec7a470cfaed1c4c34bd729027a982f
mohanson <mohanson@outlook.com>
2020-03-05 13:06:00 +0800

    Submit for stash 52

--- cmd/make/main.go
+++ cmd/make/main.go
@@ -5,11 +5,18 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
 var (
-	cCompile = flag.String("gcc", "/root/app/riscv/bin/riscv64-unknown-elf-gcc", "")
+	cTmp = flag.String("tmp", "/tmp", "")
+	cGCC = flag.String("gcc", filepath.Join(cRiscvTool, "bin", "riscv64-unknown-elf-gcc"), "")
+)
+
+var (
+	cPwd, _    = os.Getwd()
+	cRiscvTool = os.Getenv("RISCV")
 )
 
 func call(name string, arg ...string) {
@@ -22,10 +29,30 @@ func call(name string, arg ...string) {
 	}
 }
 
-func main() {
-	flag.Parse()
-	os.Mkdir("build", 0755)
-	os.Mkdir("build/res", 0755)
-	os.Mkdir("build/res/program", 0755)
-	call(*cCompile, "-o", "./build/res/program/minimal", "./res/program/minimal.c")
+func makeRiscvTests() {
+	os.Chdir(*cTmp)
+	if _, err := os.Stat("riscv-tests"); err == nil {
+		return
+	}
+	call("git", "clone", "https://github.com/nervosnetwork/riscv-tests")
+	os.Chdir("riscv-tests")
+	call("git", "submodule", "update", "--init", "--recursive")
+	call("autoconf")
+	call("./configure", "--prefix="+cRiscvTool)
+	call("make", "isa")
 }
+
+func main() {
+	if cRiscvTool == "" {
+		log.Panicln("$RISCV undefined")
+	}
+	flag.Parse()
+	if flag.Arg(0) == "test" {
+		makeRiscvTests()
+	}
+}
+
+// os.Mkdir("build", 0755)
+// os.Mkdir("build/res", 0755)
+// os.Mkdir("build/res/program", 0755)
+// call(*cCompile, "-o", "./build/res/program/minimal", "./res/program/minimal.c")
