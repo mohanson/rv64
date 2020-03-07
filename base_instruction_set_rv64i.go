61fb2d1ac4e994c28f1523472ad5429ed7a45dfa
mohanson <mohanson@outlook.com>
2020-03-07 20:48:41 +0800

    Submit for stash 0a

--- base_instruction_set_rv64i.go
+++ base_instruction_set_rv64i.go
@@ -354,43 +354,38 @@ func ExecuterRV64I(c *CPU, i uint64) (uint64, error) {
 		rd, rs1, csr := IType(i)
 		DebuglnIType("CSRRS", rd, rs1, csr)
 		c.SetRegister(rd, c.GetCSR(csr))
-		if rs1 != Rzero {
-			c.SetCSR(csr, c.GetCSR(csr)|c.GetRegister(rs1))
-		}
+		c.SetCSR(csr, c.GetCSR(csr)|c.GetRegister(rs1))
 		c.SetPC(c.GetPC() + 4)
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0011_0000_0111_0011: // CSRRC
 		rd, rs1, csr := IType(i)
 		DebuglnIType("CSRRC", rd, rs1, csr)
 		c.SetRegister(rd, c.GetCSR(csr))
-		if rs1 != Rzero {
-			c.SetCSR(csr, c.GetCSR(csr)&(math.MaxUint64-c.GetRegister(rs1)))
-		}
+		c.SetCSR(csr, c.GetCSR(csr)&(math.MaxUint64-c.GetRegister(rs1)))
 		c.SetPC(c.GetPC() + 4)
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0101_0000_0111_0011: // CSRRWI
 		rd, rs1, csr := IType(i)
+		rs1 = SignExtend(rs1, 4)
 		DebuglnIType("CSRRWI", rd, rs1, csr)
 		c.SetRegister(rd, c.GetCSR(csr))
-		c.SetCSR(csr, uint64(rs1))
+		c.SetCSR(csr, rs1)
 		c.SetPC(c.GetPC() + 4)
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0110_0000_0111_0011: // CSRRSI
 		rd, rs1, csr := IType(i)
+		rs1 = SignExtend(rs1, 4)
 		DebuglnIType("CSRRSI", rd, rs1, csr)
 		c.SetRegister(rd, c.GetCSR(csr))
-		if rs1 != Rzero {
-			c.SetCSR(csr, c.GetCSR(csr)|uint64(rs1))
-		}
+		c.SetCSR(csr, c.GetCSR(csr)|rs1)
 		c.SetPC(c.GetPC() + 4)
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0111_0000_0111_0011: // CSRRCI
 		rd, rs1, csr := IType(i)
+		rs1 = SignExtend(rs1, 4)
 		DebuglnIType("CSRRCI", rd, rs1, csr)
 		c.SetRegister(rd, c.GetCSR(csr))
-		if rs1 != Rzero {
-			c.SetCSR(csr, c.GetCSR(csr)&(math.MaxUint64-uint64(rs1)))
-		}
+		c.SetCSR(csr, c.GetCSR(csr)&(math.MaxUint64-rs1))
 		c.SetPC(c.GetPC() + 4)
 		return 1, nil
 	case i&0b_0000_0000_0000_0000_0111_0000_0111_1111 == 0b_0000_0000_0000_0000_0110_0000_0000_0011: // LWU
