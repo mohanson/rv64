dd2565f22404a70a396881a71499c6a0912bf158
mohanson <mohanson@outlook.com>
2020-03-11 22:38:16 +0800

    Submit for stash 06

--- cpu_pipeline.go
+++ cpu_pipeline.go
@@ -1,5 +1,20 @@
 package rv64
 
+// The classic RISC pipeline comprises:
+//   1. Instruction fetch
+//   2. Instruction decode and register fetch
+//   3. Execute
+//   4. Memory access
+//   5. Register write back
+//
+// | 1   | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   |
+// | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+// | IF  | ID  | EX  | MEM | WB  |     |     |     |     |
+// |     | IF  | ID  | EX  | MEM | WB  |     |     |     |
+// |     |     | IF  | ID  | EX  | MEM | WB  |     |     |
+// |     |     |     | IF  | ID  | EX  | MEM | WB  |     |
+// |     |     |     |     | IF  | ID  | EX  | MEM | WB  |
+
 func (c *CPU) PipelineInstructionFetch() ([]byte, error) {
 	a, err := c.GetMemory().GetByte(c.GetPC(), 2)
 	if err != nil {
