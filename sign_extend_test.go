b04b2c0ab25e9ccba743096d89dedbb5af03ec8c
mohanson <mohanson@outlook.com>
2020-04-13 11:06:02 +0800

    Submit for stash 16

--- sign_extend_test.go
+++ sign_extend_test.go
@@ -8,7 +8,7 @@ func TestSignExtend(t *testing.T) {
 	data := [][]uint64{
 		{0x0000000000000005, 0x02, 0xfffffffffffffffd},
 		{0x0000000000000005, 0x03, 0x0000000000000005},
-		{0x8000000000000005, 0x40, 0x8000000000000005},
+		{0x8000000000000005, 0x3f, 0x8000000000000005},
 		{0x80000000000000a5, 0x07, 0xffffffffffffffa5},
 	}
 	for _, l := range data {
