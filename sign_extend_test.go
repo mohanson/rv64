0c0ecdb3af3aeea2014f8fc1bbf8e116e339d580
mohanson <mohanson@outlook.com>
2020-04-13 11:03:57 +0800

    Submit for stash 15

--- sign_extend_test.go
+++ sign_extend_test.go
@@ -6,10 +6,10 @@ import (
 
 func TestSignExtend(t *testing.T) {
 	data := [][]uint64{
-		[]uint64{0x0000000000000005, 0x02, 0xfffffffffffffffd},
-		[]uint64{0x0000000000000005, 0x03, 0x0000000000000005},
-		[]uint64{0x8000000000000005, 0x40, 0x8000000000000005},
-		[]uint64{0x80000000000000a5, 0x07, 0xffffffffffffffa5},
+		{0x0000000000000005, 0x02, 0xfffffffffffffffd},
+		{0x0000000000000005, 0x03, 0x0000000000000005},
+		{0x8000000000000005, 0x40, 0x8000000000000005},
+		{0x80000000000000a5, 0x07, 0xffffffffffffffa5},
 	}
 	for _, l := range data {
 		t.Log(SignExtend(l[0], l[1]), l[2])
